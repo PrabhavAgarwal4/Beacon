7aaf9defd070ef29313db6395a0b00561d5914c2
PrabhavAgarwal4 <prabhavagrawal1@gmail.com>
2026-03-26 00:27:38 +0530

    Add loginUser,getUser,getPendingUsers,approveUser controllers and verifyJWT,isAdmin middlewares and generate Access/Refresh token

---
+++ Backend/src/controllers/admin.controller.js
@@ -0,0 +1,36 @@
+import {asyncHandler} from "../utils/asyncHandler.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import { ApiError } from "../utils/ApiError.js"
+import { pool } from "../config/postgres.js"
+
+
+const getPendingUsers = asyncHandler(async(req,res)=>{
+    const result = await pool.query(
+        "SELECT id,name,email,role FROM users where status='PENDING'"
+    )
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,result.rows,"Pending users fetched successfully")
+    )
+})
+
+const approveUser = asyncHandler(async(req,res)=>{
+    const {userId} = req.body
+    if(!userId){
+        throw new ApiError(400,"User id is required")
+    }
+    const user = await pool.query(
+        "UPDATE users SET status='' WHERE id=$1"[userId]
+    )
+    if(!user){
+        throw new ApiError(404,"User not found")
+    }
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,{approved:true},"User approved")
+    )
+})
+
+export {getPendingUsers,approveUser}
---
+++ Backend/src/controllers/user.controller.js
@@ -0,0 +1,104 @@
+import {asyncHandler} from "../utils/asyncHandler.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import { ApiError } from "../utils/ApiError.js"
+import pool from "../config/postgres.js"
+import bcrypt from "bcrypt"
+import { generateAccessToken, generateRefreshToken } from "../utils/jwt.js";
+
+
+
+const registerUser = asyncHandler(async(req,res)=>{
+    //get details from req.body
+    //verify details for requirements
+    //hash password
+    //save user in database
+    //send for admin approval and send res 
+
+    const {name,email,password,role} = req.body //get details
+
+    //verify details
+    if(!name?.trim() || !email?.trim() || !password?.trim() || !role?.trim()){  
+        throw new ApiError(400,"All fields are required")
+    }
+    if(password.length < 7){
+        throw new ApiError(400,"Length of password must be atleast 7")
+    }
+
+    const password_hash = await bcrypt.hash(password,10) //hash password
+
+    const result = await pool.query(
+        `INSERT INTO users (name, email, password_hash, role, status)
+        VALUES ($1,$2,$3,$4,'PENDING') RETURNING id,name,email,role,status`,
+        [name, email, password_hash, role]
+    )
+    
+    if(!result){
+        throw new ApiError(400,"User registration unsuccessfull")
+    }
+
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(201,"User registered,waiting for admin approval")
+    )
+})
+
+const loginUser = asyncHandler(async(req,res)=>{
+    //user already veerified whether approved or not using verifyJWT
+    const {email,password} = req.body
+
+    if(!email?.trim() || !password?.trim()){
+        throw new ApiError(400,"All fields are required")
+    }
+
+    const result = await pool.query(
+        'SELECT * FROM users WHERE email=$1'[email]
+    )
+    
+    const user = result.rows[0]
+    if(!user){
+        throw new ApiError(400,"User not found")
+    }
+    if(user.status !== "APPROVED"){
+        throw new ApiError(403,"User not approved by admin")
+    }
+    if (!user.is_active) {
+        throw new ApiError(403, "Account disabled");
+    }
+    
+    const isMatch = await bcrypt.compare(password,user.password_hash)
+    if(!isMatch){
+        throw new ApiError(400,"Invalid email or password")
+    }
+    
+    const accessToken = generateAccessToken(user)
+    const refreshToken = generateRefreshToken(user)
+
+    //add refresh token in user 
+    await pool.query(
+        "UPDATE users SET refresh_token=$1 WHERE id=$2"[refreshToken,user.id]
+    )
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,{accessToken,refreshToken},"Logged in successfully")
+    )
+})
+
+const getUser = asyncHandler(async (req, res) => {
+  const {userId} = req.body
+
+  if(!userId){
+    throw new ApiError(400,"User not found")
+  }
+
+  const result = await pool.query(
+    "SELECT id,name,email,role,status FROM users WHERE id=$1",
+    [userId]
+  );
+
+  return res.json(new ApiResponse(200, result.rows[0], "User fetched"));
+});
+
+export {registerUser,loginUser,getUser}
