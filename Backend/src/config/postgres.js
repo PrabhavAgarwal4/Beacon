daa3515969494bfbd85b34518bb19b94fcbe176b
PrabhavAgarwal4 <prabhavagrawal1@gmail.com>
2026-03-26 10:32:17 +0530

    Add connectDB

--- Backend/src/config/postgres.js
+++ Backend/src/config/postgres.js
@@ -1,14 +1,10 @@
 import pkg from "pg"
 const { Pool } = pkg
 
+
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
 });
-
-//test connection
-pool.connect()
-  .then(() => console.log("PostgreSQL connected "))
-  .catch(err => console.error("DB connection error ", err))
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,10 +1,11 @@
-import dotenv from "dotenv"
-import { app } from "./app.js"
+import dotenv from "dotenv";
+import { app } from "./app.js";
+import { connectDB } from "./config/db.js";
 
-dotenv.config({
-    path:"./.env"
-})
+dotenv.config();
 
-app.listen(process.env.PORT || 3000 ,()=>{
-    console.log(`Server is running at PORT:${process.env.PORT}`)
-})
+connectDB().then(() => {
+  app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server running at PORT:${process.env.PORT}`);
+  });
+});
