e84f26ad8d3e1440182a96619b9bc2623deb8ab1
PrabhavAgarwal4 <prabhavagrawal1@gmail.com>
2026-03-26 14:05:50 +0530

    Add setProfile,getProfile controllers

---
+++ Backend/src/controllers/student.controller.js
@@ -0,0 +1,49 @@
+import { asyncHandler } from "../utils/asyncHandler";
+import { ApiError } from "../utils/ApiError";
+import { ApiResponse } from "../utils/ApiResponse";
+import pool from "../config/postgres";
+
+const setProfile = asyncHandler(async(req,res)=>{
+   const userId = req.user.id 
+   const {rollno,department,course,graduation_year,cgpa,phone} = req.body
+
+   if(!rollno || !department?.trim() || !course?.trim() || !phone?.trim() || !graduation_year || !cgpa){
+      throw new ApiError(400,"All fields are required")
+   }
+
+   const student = await pool.query(
+        "INSERT INTO student_details (user_id,rollno,department,course,graduation_year,cgpa,phone) VALUES ($1,$2,$3,$4,$5,$6,$7) RETURNING id,rollno,department,course,graduation_year,cgpa",[userId,rollno,department,course,graduation_year,cgpa,phone]
+   )
+
+   if(!student){
+      throw new ApiError(400,"Student details failed to save")
+   }
+
+   return res
+   .status(201)
+   .json(
+    new ApiResponse(201,"Student details saved")
+   )
+})
+
+
+const getProfile = asyncHandler(async(req,res)=>{
+   const userId = req.user.id
+ 
+   const student = await pool.query(
+    'SELECT rollno,department,course,graduation_year,cgpa,phone FROM student_details WHERE user_id=$1',[userId]
+   )
+
+   if(!student){
+    throw new ApiError(404,"Student not found")
+   }
+
+   return res
+   .status(200)
+   .json(
+    new ApiResponse(200,{student},"Student details fetched")
+   )
+})
+
+
+export {setProfile,getProfile}
