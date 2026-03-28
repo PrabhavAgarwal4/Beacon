00a80c24e7d8a9f42fe0a32cb1256680d526b6ac
PrabhavAgarwal4 <prabhavagrawal1@gmail.com>
2026-03-28 11:27:03 +0530

    Add rate limiter using redis

--- Backend/src/routes/application.routes.js
+++ Backend/src/routes/application.routes.js
@@ -14,7 +14,7 @@ const router = Router()
 
 router.use(verifyJWT)
 
-router.route("/apply/:jobId").post(rateLimiter(3,3600),applyToJob)                 //student
+router.route("/apply/:jobId").post(rateLimiter(5,900),applyToJob)      //student(ratelimiter -> 5 applications every 15mins)
 router.route("/my-applications").get(getMyApplications)        //student
 router.route("/job/:jobId/applicants").get(getApplicantsForJob)    //admin,recruiter
 router.route("/update-status/:applicationId").post(updateApplicationStatus)   //recruiter
