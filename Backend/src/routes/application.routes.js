bca526d9ede042f99db4626908eb2c48c4169d73
PrabhavAgarwal4 <prabhavagrawal1@gmail.com>
2026-03-28 11:09:54 +0530

    Add redis

--- Backend/src/routes/application.routes.js
+++ Backend/src/routes/application.routes.js
@@ -6,6 +6,7 @@ import {
   getApplicantsForJob,
   updateApplicationStatus
 } from "../controllers/application.controller.js";
+import rateLimiter from "../middlewares/rateLimiter.js";
 
 
 const router = Router()
@@ -13,7 +14,7 @@ const router = Router()
 
 router.use(verifyJWT)
 
-router.route("/apply/:jobId").post(applyToJob)                 //student
+router.route("/apply/:jobId").post(rateLimiter(3,3600),applyToJob)                 //student
 router.route("/my-applications").get(getMyApplications)        //student
 router.route("/job/:jobId/applicants").get(getApplicantsForJob)    //admin,recruiter
 router.route("/update-status/:applicationId").post(updateApplicationStatus)   //recruiter
--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,12 +1,12 @@
 import {Router} from "express"
 import { getUser, loginUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js"
-
+import rateLimiter from "../middlewares/rateLimiter.js"
 
 const router = Router()
 
 router.route("/register").post(registerUser)
-router.route("/login").post(loginUser)
+router.route("/login").post(rateLimiter(5,60),loginUser)
 router.route("/me").get(verifyJWT,getUser)
 router.route("/refreshToken").post(refreshAccessToken)
 
