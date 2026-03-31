9f216e1b829f3883a645e14466c799e77f9de873
PrabhavAgarwal4 <prabhavagrawal1@gmail.com>
2026-03-31 23:07:03 +0530

    Fix login page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,21 +1,33 @@
 import './App.css'
-import {BrowserRouter,Routes,Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/auth/Home.jsx";
 import Login from "./pages/auth/Login.jsx";
 import Register from "./pages/auth/Register.jsx";
+import ProtectedRoute from './routes/ProtectedRoute.jsx';
 
 function App() {
-  
-
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/login' element={<Login/>}/>
-      <Route path="/register" element={<Register/>}/>
-    </Routes>
+      <Routes>
+        {/* Protected Routes */}
+        <Route 
+          path="/" 
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          } 
+        />
+
+        {/* Public Routes */}
+        <Route path='/login' element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        
+        {/* 404 Catch-all */}
+        <Route path="*" element={<div>404 - Page Not Found</div>} />
+      </Routes>
     </BrowserRouter>
   )
 }
 
-export default App
+export default App;
