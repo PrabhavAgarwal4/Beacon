0f6739aaf6153e5f5071a15a1e975be4e1b1615f
PrabhavAgarwal4 <prabhavagrawal1@gmail.com>
2026-03-31 19:07:04 +0530

    Add pages(skeleton) and axios

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
-
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Home from "./pages/Home";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      <h1 class="underline ">
-    Hello world!
-  </h1>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path="/register" element={<Register/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
