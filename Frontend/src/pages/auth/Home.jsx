7917290cef074181d8517b1d9ad93933453d8cee
PrabhavAgarwal4 <prabhavagrawal1@gmail.com>
2026-03-31 23:43:25 +0530

    Add logout button on home page

--- Frontend/src/pages/auth/Home.jsx
+++ Frontend/src/pages/auth/Home.jsx
@@ -1,19 +1,39 @@
-import React, { useContext } from 'react'
-import {AuthContext} from "../../context/authContext.jsx"
+import { useContext } from "react";
+import { AuthContext } from "../../context/authContext.jsx"
+import { logoutUser } from "../../services/authService.js";
+import { useNavigate } from "react-router-dom";
 
-const Home = () => {
-  const {user,loading} = useContext(AuthContext)
-  if(loading) return <h2>Loading...</h2>
+function Home() {
+  const { user, setUser } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await logoutUser();
+
+      setUser(null);
+      localStorage.removeItem("user");
+
+      navigate("/login");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <h1>Home</h1>
+
       {user ? (
-        <h2>Welcome, {user.name}</h2>
+        <>
+          <h2>Welcome, {user.name}</h2>
+          <button onClick={handleLogout}>Logout</button>
+        </>
       ) : (
         <h2>Not logged in</h2>
       )}
     </div>
-  )
+  );
 }
 
 export default Home
--- Frontend/src/services/authService.js
+++ Frontend/src/services/authService.js
@@ -3,3 +3,4 @@ import api from "./api"
 export const loginUser = (data) => api.post("/users/login", data);
 export const registerUser = (data) => api.post("/users/register", data);
 export const getUser = () => api.get("/users/me")
+export const logoutUser = () => api.post("/users/logout")
